b78b3333a22f17eb6a5996e9953e5d29383a723d
Maraki-Astatke <kikimaki2121@gmail.com>
2026-05-05 22:30:32 +0300

    app

--- backend/src/app.js
+++ backend/src/app.js
@@ -31,6 +31,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
   'http://localhost:5001',
+  'https://booking-app-1tq7.vercel.app/',
   process.env.CLIENT_URL,
   process.env.FRONTEND_URL,
   process.env.RAILWAY_URL
